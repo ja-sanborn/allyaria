914963da932cf38f8b3d7421a14ef1dd1fbe5cfb
JA Sanborn <jsanborn@simplicitypoint.com>
2025-10-03 21:09:43 -0700

    Style Helper with Tests

---
+++ src/Allyaria.Theming/Helpers/StyleHelper.cs
@@ -0,0 +1,33 @@
+﻿using Allyaria.Theming.Contracts;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Allyaria.Theming.Helpers;
+
+/// <summary>Provides helper extensions for building CSS style declarations from theme values.</summary>
+internal static class StyleHelper
+{
+    /// <summary>Appends a CSS declaration for the given property and value to a <see cref="StringBuilder" />.</summary>
+    /// <param name="builder">The <see cref="StringBuilder" /> used to collect CSS declarations.</param>
+    /// <param name="value">The theming value containing the CSS representation.</param>
+    /// <param name="propertyName">The CSS property name to apply (e.g., "color").</param>
+    /// <param name="varPrefix">
+    /// The prefix used when generating a CSS variable name. Results in a property of the form
+    /// <c>--{varPrefix}-var-{propertyName}</c>. Hyphens and whitespace are normalized.
+    /// </param>
+    public static void ToCss(this StringBuilder builder, ValueBase value, string propertyName, string varPrefix)
+    {
+        if (string.IsNullOrWhiteSpace(propertyName) || string.IsNullOrWhiteSpace(value.Value))
+        {
+            return;
+        }
+
+        var prefix = Regex.Replace(varPrefix, @"[\s-]+", "-").Trim('-').ToLowerInvariant();
+
+        var prefixedProperty = string.IsNullOrWhiteSpace(prefix)
+            ? propertyName
+            : $"--{prefix}-var-{propertyName}";
+
+        builder.Append(value.ToCss(prefixedProperty));
+    }
+}
---
+++ tests/Allyaria.Theming.UnitTests/Helpers/StyleHelperTests.cs
@@ -0,0 +1,99 @@
+﻿using Allyaria.Theming.Helpers;
+using System.Text;
+
+namespace Allyaria.Theming.UnitTests.Helpers;
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+public sealed class StyleHelperTests
+{
+    [Fact]
+    public void ToCss_Should_AppendCssFromValue_When_AllInputsValid_And_PrefixIsNormalized()
+    {
+        // Arrange
+        var builder = new StringBuilder();
+        var value = new AllyariaStringValue("some-theme-value");
+        var propertyName = "color";
+        var varPrefix = "  My--Theme   Name  ";
+        var expectedPrefixedProperty = "--my-theme-name-var-color";
+        var expectedCss = $"{expectedPrefixedProperty}:{value.Value};";
+
+        // Act
+        builder.ToCss(value, propertyName, varPrefix);
+
+        // Assert
+        builder.ToString().Should().Be(expectedCss);
+    }
+
+    [Theory]
+    [InlineData("----Fancy----", "--fancy-var")]
+    [InlineData("- - Fancy - -", "--fancy-var")]
+    [InlineData("A  B   C", "--a-b-c-var")]
+    [InlineData("Title-Case Prefix", "--title-case-prefix-var")]
+    public void ToCss_Should_BuildCssVariableName_With_LowercasedHyphenNormalizedPrefix(string varPrefix,
+        string expectedPrefixPortion)
+    {
+        // Arrange
+        var builder = new StringBuilder();
+        var value = new AllyariaStringValue("ok");
+        var propertyName = "border";
+        var expectedPrefixedProperty = $"{expectedPrefixPortion}-{propertyName}";
+        var expectedCss = $"{expectedPrefixedProperty}:{value.Value};";
+
+        // Act
+        builder.ToCss(value, propertyName, varPrefix);
+
+        // Assert
+        builder.ToString().Should().Be(expectedCss);
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("   ")]
+    public void ToCss_Should_DoNothing_When_PropertyNameIsNullOrWhitespace(string? propertyName)
+    {
+        // Arrange
+        var builder = new StringBuilder();
+        var value = new AllyariaStringValue("exists");
+
+        // Act
+        builder.ToCss(value, propertyName!, "prefix");
+
+        // Assert
+        builder.ToString().Should().BeEmpty();
+    }
+
+    [Fact]
+    public void ToCss_Should_ThrowNullReferenceException_When_BuilderIsNull_And_InputsValid()
+    {
+        // Arrange
+        StringBuilder? builder = null;
+        var value = new AllyariaStringValue("present");
+        var propertyName = "color";
+        var varPrefix = "the-prefix";
+
+        // Act
+        var act = () => builder!.ToCss(value, propertyName, varPrefix);
+
+        // Assert
+        act.Should().Throw<NullReferenceException>();
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("   ")]
+    public void ToCss_Should_UsePropertyNameDirectly_When_VarPrefixIsNullOrWhitespace(string varPrefix)
+    {
+        // Arrange
+        var builder = new StringBuilder();
+        var value = new AllyariaStringValue("red");
+        var propertyName = "background";
+        var expectedCss = $"{propertyName}:{value.Value};";
+
+        // Act
+        builder.ToCss(value, propertyName, varPrefix!);
+
+        // Assert
+        builder.ToString().Should().Be(expectedCss);
+    }
+}
